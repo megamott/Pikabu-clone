c3d9ac945d371e74ff774cbfdc1ac2570ca28a3e
megamott <megamott27@gmail.com>
2021-08-31 13:27:24 +0300

    Add comment model

--- pikabu_clone/apps/posts/models.py
+++ pikabu_clone/apps/posts/models.py
@@ -26,3 +26,35 @@ class Post(models.Model):
     class Meta:
         verbose_name = 'post'
         verbose_name_plural = 'Posts'
+
+
+class Comment(models.Model):
+    """ Comment under the post """
+
+    body = models.TextField(verbose_name='main comment text')
+    author = models.ForeignKey(
+        User,
+        db_index=True,
+        on_delete=models.CASCADE,
+        verbose_name='which user the post belongs to'
+    )
+    post = models.ForeignKey(
+        Post,
+        db_index=True,
+        on_delete=models.CASCADE,
+        verbose_name='which post the comment belongs to'
+    )
+    parent_comment = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name='parent comment, to achieve tree comments behavior'
+    )
+
+    def __str__(self):
+        return f'{self.body}: {self.post} by {self.author}'
+
+    class Meta:
+        verbose_name = 'comment'
+        verbose_name_plural = 'Comments'
